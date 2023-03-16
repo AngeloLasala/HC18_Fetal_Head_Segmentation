ee98d81237aaf9080dd6e825d7d16d715cd106c1
lasala.angelo96@gmail.com <lasala.angelo96@gmail.com>
2023-03-16 15:02:26 +0100

    final code of seg_gan

--- HC_18_Fetal_Head_Segmentation/segmentation_gan.py
+++ HC_18_Fetal_Head_Segmentation/segmentation_gan.py
@@ -3,8 +3,10 @@ Apply the segmentation model on the sample for GAN
 """
 import argparse
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from PIL import Image
 import copy
 
 from segmentation import normalize
@@ -73,10 +75,11 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	## images_path
-	gan_path = 'Dataset/train_TV'
+	gan_path = 'Dataset/train_TC'
 	save_folder = 'Model/' + args.folder_name
 	
-	gan_list = [gan_path + '/' + i for i in os.listdir(gan_path)]
+	gan_list = [gan_path + '/test' + '/' + i for i in os.listdir(gan_path + '/test')]
+	gan_list.sort() ## IMPORTANT for the enumeration 
 	
 	gan_dataset = tf.data.Dataset.list_files(gan_list, shuffle=False)
 	gan_dataset = gan_dataset.map(load_sample_test,
@@ -92,11 +95,16 @@ if __name__ == '__main__':
 	pred[pred < 0.5] = 0
 
 
-	gan_result = gan_path + '/segmentation'
+	gan_result = gan_path + '/segmentation_test'
+	raw_segmentation_path = gan_path + '/raw_segmentation_test'
+	mask_path = gan_path + '/mask_test'
 	smart_makedir(gan_result)
+	smart_makedir(raw_segmentation_path)
+	smart_makedir(mask_path)
 	for i,(image,cam) in enumerate(iter(gan_dataset.take(len(gan_list)))):
-	
-		fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,6), num=f'US image and mark, gan {i}')
+		sample_num = gan_list[i].split('/')[-1].split('.')[0].split('_')[-1]
+		print(sample_num)
+		fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,6), num=f'US image and mark, gan {sample_num}')
 		ax[0].imshow(image[0,:,:,:], cmap='gray')
 		ax[0].set_title('US fetal head')
 		ax[0].axis('off')
@@ -104,6 +112,19 @@ if __name__ == '__main__':
 		ax[1].imshow(pred[i,:,:,:], cmap='gray')
 		ax[1].set_title('Segmentation mask')
 		ax[1].axis('off')
-		plt.savefig(gan_result + '/' + f'US image and mark, gan {i}')
-		# plt.show()
+		plt.savefig(gan_result + '/' + f'US image and mark, gan {sample_num}')
+		plt.close()
+
+		fig1, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(6,6), num=f'Raw Segmentation {sample_num}')
+		ax1.imshow(image[0,:,:,:], cmap='gray')
+		ax1.imshow(pred[i,:,:,:], cmap='jet', alpha = 0.5)
+		ax1.axis('off')
+		plt.savefig(raw_segmentation_path + '/' + f'Raw Segmentation {sample_num}')
+		plt.close()
+
+		#save only mask for ellipses fitting
+		im = Image.fromarray((pred[i,:,:,0]*255).astype(np.uint8))
+		im.save(mask_path + f'/mask_{sample_num}.png')
+		
+		plt.show()
 
